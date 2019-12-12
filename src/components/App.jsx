9aaae5b5ee007721ad614252e9100354dd0f381f
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-12 21:57:17 +1100

    final but need to link to the next lesson

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+// declaritive programming
+// function Strike() {
+//   document.getElementById("root").style.textDecoration = "line-through";
+// }
+
+// function unStrike() {
+//   document.getElementById("root").style.textDecoration = null;
+// }
+
+// function App() {
+//   // var isDone = true;
+//   // const strikeThrough = { textDecoration: "line-through" };
+//   // return <p style={isDone ? strikeThrough : null}>Buy milk</p>;
+//   return (
+//     <div>
+//       <p>Buy milk</p>
+//       <button onClick={Strike}>Change to strike through</button>
+//       <button onClick={unStrike}>Unstrike</button>
+//     </div>
+//   );
+// }
+
+// imperative programming
+
+var isDone = false;
+function Strike() {
+  isDone = true;
+}
+
+function unStrike() {
+  isDone = false;
+}
+
 function App() {
-  return <p>Buy milk</p>;
+  // var isDone = true;
+  // const strikeThrough = { textDecoration: "line-through" };
+  // return <p style={isDone ? strikeThrough : null}>Buy milk</p>;
+  return (
+    <div>
+      <p>Buy milk</p>
+      <button onClick={Strike}>Change to strike through</button>
+      <button onClick={unStrike}>Unstrike</button>
+    </div>
+  );
 }
 
 export default App;
